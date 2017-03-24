f7ff0b22e619ac7d8cb7309073bfc15d863e820b
YingboMa <mayingbo5@gmail.com>
2017-03-24 19:15:15 -0400

    Generalize Shooting methods solver (u0 dimensions and OptSolver)

--- src/BoundaryValueDiffEq.jl
+++ src/BoundaryValueDiffEq.jl
@@ -1,5 +1,37 @@
 module BoundaryValueDiffEq
 
-# package code goes here
+# using DiffEqBase, OrdinaryDiffEq
+# import DiffEqBase: solve
+using DifferentialEquations
+using Optim
+
+abstract AbstractBVProblem{dType,bType,isinplace,F} <: DEProblem
+
+type BVProblem{dType,bType,initType,F} <: AbstractBVProblem{dType,bType,F}
+  f::F
+  domin::dType
+  bc::bType
+  init::initType
+end
+
+function BVProblem(f,domin,bc,init=nothing)
+  BVProblem{eltype(domin),eltype(bc),eltype(init),typeof(f)}(f,domin,bc,init)
+end
+
+function solve(prob::BVProblem; OptSolver=LBFGS())#, ODESolver...)
+  bc = prob.bc
+  u0 = bc[1]
+  len = length(bc[1])
+  probIt = ODEProblem(prob.f, u0, prob.domin)
+  function loss(minimizer)
+    probIt.u0 = minimizer
+    sol = DifferentialEquations.solve(probIt)#, ODESolver...)
+    norm(sol[end]-bc[2])
+  end
+  opt = optimize(loss, u0, OptSolver)
+  probIt.u0 = opt.minimizer
+  @show opt.minimum
+  DifferentialEquations.solve(probIt)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,24 @@
 using BoundaryValueDiffEq
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+function f(t, y, du)
+  (x, v) = y
+  du[1] = v
+  du[2] = -x
+end
+
+bc = [[0., 1.], [1., 1.]]
+domin = (0.,100.)
+bvp = BVProblem(f, domin, bc)
+@test solve(bvp)[end] ≈ bc[2]
+
+function lorenz(t,u,du)
+ du[1] = 10.0(u[2]-u[1])
+ du[2] = u[1]*(28.0-u[3]) - u[2]
+ du[3] = u[1]*u[2] - (8/3)*u[3]
+end
+
+bc_lorenz = [[1.0,0.0,0.0],[-10.,-11.,29.]]
+
+lorenzprob = BVProblem(lorenz, (0.0,1.0), bc_lorenz)
+@test norm(solve(lorenzprob)[end] - bc_lorenz[2]) < 0.5
