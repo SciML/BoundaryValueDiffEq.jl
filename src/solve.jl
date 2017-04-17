230eff994aae9479c40fc530c84f4b2579e519f7
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-04-17 08:37:48 -0700

    shield the problem type from further changes

--- src/solve.jl
+++ src/solve.jl
@@ -1,7 +1,7 @@
 # The Solve Function
 function solve(prob::BVProblem, alg::Shooting; kwargs...)
   bc = prob.bc
-  u0 = prob.init
+  u0 = deepcopy(prob.init)
   # Convert a BVP Problem to a IVP problem.
   probIt = ODEProblem(prob.f, u0, prob.domain)
   # Form a root finding function.
