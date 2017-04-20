2b3e601afedd56aee53796a44477160b63f85147
YingboMa <mayingbo5@gmail.com>
2017-04-20 17:52:33 -0400

    Fix typos and add a simple test

--- src/collocation.jl
+++ src/collocation.jl
@@ -1,8 +1,3 @@
-fun!(out, x, y, p) = begin
-   out[1] = p[2]*x*y[2]
-   out[2] = p[1]*x*-exp.(y[1])
-end
-
 @inline function eval_fun!(f_out, fun!, x, y, p)
     for i in 1:size(f_out,1)
         fun!(view(f_out, i, :), x[i], view(y,i,:), p)
@@ -18,19 +13,41 @@ end
 
 @inline function eval_col_residual!(residual, f, y, h, f_m)
     for i in 1:size(f, 1)-1
-        residual[i] = y[i+1, :] - y[i, :] - h * 1//6 * (f[i, :] + f[:, i+1] + 4 * f_m[i])
+        residual[i, :] = y[i+1, :] - y[i, :] - h * 1//6 * (f[i, :] + f[i+1, :] + 4 * f_m[i])
     end
 end
 
-function collocation_points!(f, y_m, f_m, residual, fun, x, y, p, h)
-    eval_fun!(f, fun, x, y, p)
+function collocation_points!(f, y_m, f_m, residual, fun!, x, y, p, h)
+    eval_fun!(f, fun!, x, y, p)
     # copy!(y_m, 1//2 * (y[2:end, :] + y[1:end-1, :]) - 1//8 * h * (f[2:end, :] - f[1:end-1, :]))
     eval_y_middle!(y_m, f, y, h)
-    eval_fun(f_m, x[1:end-1] + 1//2 * h, y_m, p)
-    # copy!(residual, y[2:end, :] - y[1:end-1, :] - h * 1//6 * (f[1:end-1, :] + f[:, 2:end] + 4 * f_m))
+    eval_fun!(f_m, fun!, x[1:end-1] + 1//2 * h, y_m, p)
+    # copy!(residual, y[2:end, :] - y[1:end-1, :] - h * 1//6 * (f[1:end-1, :] + f[2:end, :] + 4 * f_m))
     eval_col_residual!(residual, f, y, h, f_m)
 end
 
-function system_jac(n, m, k, i_jac, j_jac, df_dy, df_dy_m, df_dp, df_dp_m, dbc_da, dbc_db, dbc_dp)
-
+function allocate_arrays(n, m)
+    f = Array{Float64}(m,n)
+    y_m = Array{Float64}(m-1,n)
+    f_m = Array{Float64}(m-1,n)
+    residual = Array{Float64}(m-1, n)
+    f, y_m, f_m, residual
 end
+
+test_col() = begin
+    fun!(out, x, y, p) = begin
+       out[1] = p[2]*x*y[2]
+       out[2] = p[1]*x*-exp.(y[1])
+    end
+    n, m = 2,4
+    x = collect(linspace(0,1,m))
+    h = x[2]-x[1]
+    y = zeros(m,n)
+    p = ones(2)
+    f, y_m, f_m, residual = allocate_arrays(n, m)
+    collocation_points!(f, y_m, f_m, residual, fun!, x, y, p, h)
+end
+
+# function system_jac(n, m, k, i_jac, j_jac, df_dy, df_dy_m, df_dp, df_dp_m, dbc_da, dbc_db, dbc_dp)
+
+# end
