a9dd50a179328285a74025c5239a09fde3a44901
YingboMa <mayingbo5@gmail.com>
2017-06-10 20:14:53 -0400

    Add second order linear test

--- test/mirk_test.jl
+++ test/mirk_test.jl
@@ -1,7 +1,10 @@
 using Base.Test
 import BoundaryValueDiffEq:BVPSystem, eval_fun!, Φ!, MIRK_scheme
 
-function func!(x, y, out)
+const n=50
+
+# First order test
+function func_1!(x, y, out)
     out[1] = y[2]
     out[2] = 0
 end
@@ -11,10 +14,20 @@ function boundary!(residual, ua, ub)
     residual[2] = ub[1]
 end
 
-n=50
 y = vcat(collect(linspace(5,0,n))', zeros(n)')
-S = BVPSystem(func!, boundary!, collect(linspace(0,5,n)),
-              y, 4)
+S1 = BVPSystem(func_1!, boundary!, collect(linspace(0,5,n)), y, 4)
 
-sol = MIRK_scheme(S)
-@test sol.f_converged && norm(diff(diff(S.y[1, :]))) < 1e-10
+sol_1 = MIRK_scheme(S1)
+@test sol_1.f_converged && norm(diff(diff(S1.y[1, :])), Inf) < 1e-10
+
+# Secend order linear test
+function func_2!(x, y, out)
+    out[1] = y[2]
+    out[2] = -y[1]
+end
+
+S2 = BVPSystem(func_2!, boundary!, collect(linspace(0,5,n)), y, 4)
+sol2 = MIRK_scheme(S2)
+sol2_analytical(t) = 5*(cos(t) - cot(5)*sin(t))
+@test sol2.f_converged && norm(sol2_analytical.( S2.x ) - S2.y[1, :], Inf) < 1e-5
+
