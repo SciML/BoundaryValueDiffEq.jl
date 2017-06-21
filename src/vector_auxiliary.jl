2aa5fc3c367824b965595ce51d65530aa7ea39c9
YingboMa <mayingbo5@gmail.com>
2017-06-21 02:25:06 -0400

    Use in-place vector auxiliary functions

--- src/vector_auxiliary.jl
+++ src/vector_auxiliary.jl
@@ -7,13 +7,18 @@ function vector_alloc(T, M, N)
     v
 end
 
-flatten_vector{T<:AbstractArray}(V::Vector{T}) = vcat(V...)
-
-function nest_vector{T}(v::Vector{T}, M, N)
-    V = vector_alloc(T, M, N)
-    for i in eachindex(V)
-        copy!(V[i], v[(M*(i-1))+1:(M*i)])
+function flatten_vector!{T<:AbstractArray}(dest::T, src::Vector{T})
+    N = length(src)
+    M = length(src[1])
+    for i in eachindex(src)
+        dest[((i-1)*M)+1:i*M] = src[i]
     end
-    V
+end
+
+function nest_vector!{T<:AbstractArray}(dest::Vector{T}, src::T)
+    M = length(dest[1])
+    for i in eachindex(dest)
+        copy!(dest[i], src[(M*(i-1))+1:(M*i)])
+    end
 end
 
