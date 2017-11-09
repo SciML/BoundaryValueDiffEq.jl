376e5684203dfcfbac829c4b72e9420137b90518
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-11-09 08:34:56 -0800

    re-style

--- src/jacobian.jl
+++ src/jacobian.jl
@@ -11,14 +11,22 @@ end
 (p::BVPJacobianWrapper)(u) = (resid = similar(u); p.loss(u,resid); resid)
 
 function ConstructSparseJacobian(f!::BVPJacobianWrapper, S::BVPSystem)
-    fg!(x::Vector, fx::Vector, gx) = RealOrComplex = eltype(fx) <: Complex ? Val{:Complex} : Val{:Real}; DiffEqDiffTools.finite_difference_jacobian!(gx, f!, x, Val{:central}, RealOrComplex, Val{:JacobianWrapper}, fx)
+    function fg!(x::Vector, fx::Vector, gx)
+        RealOrComplex = (eltype(fx) <: Complex) ? Val{:Complex} : Val{:Real}
+        DiffEqDiffTools.finite_difference_jacobian!(gx, f!, x, Val{:central},
+                        RealOrComplex, Val{:JacobianWrapper}, fx)
+    end
     g!(x::Vector, gx::Array)  = (fx = similar(x); fg!(x, fx, gx))
     J = bzeros(eltype(S.y[1]), S.M*S.N, S.M*S.N, S.M-1, S.M-1)
     NLsolve.DifferentiableMultivariateFunction(f!.loss, g!, fg!, sparse(J))
 end
 
 function ConstructJacobian(f!::BVPJacobianWrapper, S::BVPSystem)
-    fg!(x::Vector, fx::Vector, gx) = RealOrComplex = eltype(fx) <: Complex ? Val{:Complex} : Val{:Real}; DiffEqDiffTools.finite_difference_jacobian!(gx, f!, x, Val{:central}, RealOrComplex, Val{:JacobianWrapper}, fx)
+    function fg!(x::Vector, fx::Vector, gx)
+        RealOrComplex = (eltype(fx) <: Complex) ? Val{:Complex} : Val{:Real}
+        DiffEqDiffTools.finite_difference_jacobian!(gx, f!, x, Val{:central},
+                        RealOrComplex, Val{:JacobianWrapper}, fx)
+    end
     g!(x::Vector, gx::Array)  = (fx = similar(x); fg!(x, fx, gx))
     NLsolve.DifferentiableMultivariateFunction(f!.loss, g!, fg!)
 end
