790f57695f274db73f1a134034b8f2f45b2c5bce
MatFi <git@endof.de>
2021-01-13 20:54:42 +0100

    Rename type variables

--- src/collocation.jl
+++ src/collocation.jl
@@ -10,16 +10,16 @@ end
 function BVPSystem(fun, bc, p, x, y, order)
     T, U = eltype(x), eltype(y)
     M, N = size(y)
-    BVPSystem{T,U}(order, M, N, fun, bc, p, x, y, vector_alloc(U, M, N), vector_alloc(U, M, N), typeof(x)(M))
+    BVPSystem{T,U}(order, M, N, fun, bc, p, x, y, vector_alloc(T, M, N), vector_alloc(T, M,  N), eltype(y)(M))
 end
 
 # Dispatch aware of eltype(x) != eltype(prob.u0)
 function BVPSystem(prob::BVProblem, x, order)
-    U = eltype(prob.u0)
+    Y = eltype(prob.u0)
     M = length(prob.u0)
     N = size(x,1)
-    y = vector_alloc(U, M, N)
-    BVPSystem(order, M, N, prob.f, prob.bc, prob.p, x, y, vector_alloc(U, M, N), vector_alloc(U, M, N), typeof(x)(undef,M))
+    y = vector_alloc(Y, M, N)
+    BVPSystem(order, M, N, prob.f, prob.bc, prob.p, x, y, vector_alloc(Y, M, N), vector_alloc(Y, M, N), typeof(x)(undef,M))
 end
 
 # Auxiliary functions for evaluation
