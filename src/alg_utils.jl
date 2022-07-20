a388716e960b9fd1e3dac435bb427ccc0a8cd11c
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-07-20 12:18:48 -0400

    format

--- src/alg_utils.jl
+++ src/alg_utils.jl
@@ -4,4 +4,4 @@ alg_order(alg::GeneralMIRK4) = 4
 SciMLBase.isautodifferentiable(::BoundaryValueDiffEqAlgorithm) = true
 SciMLBase.allows_arbitrary_number_types(::BoundaryValueDiffEqAlgorithm) = true
 SciMLBase.allowscomplex(alg::BoundaryValueDiffEqAlgorithm) = true
-SciMLBase.isadaptive(alg::Union{MIRK4,GeneralMIRK4}) = false
+SciMLBase.isadaptive(alg::Union{MIRK4, GeneralMIRK4}) = false
