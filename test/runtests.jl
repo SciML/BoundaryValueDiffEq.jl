54e75da23bc362fd99d24a0842e8d7dfca917991
Avik Pal <avikpal@mit.edu>
2023-10-11 22:18:02 -0400

    Split up the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,41 +1,49 @@
 using Test, SafeTestsets
 
+const GROUP = uppercase(get(ENV, "GROUP", "ALL"))
+
 @testset "Boundary Value Problem Tests" begin
-    @time @testset "Shooting Method Tests" begin
-        @time @safetestset "Shooting Tests" begin
-            include("shooting/shooting_tests.jl")
-        end
-        @time @safetestset "Ray Tracing BVP" begin
-            include("shooting/ray_tracing.jl")
-        end
-        @time @safetestset "Orbital" begin
-            include("shooting/orbital.jl")
-        end
-    end
-
-    @time @testset "Collocation Method (MIRK) Tests" begin
-        @time @safetestset "Ensemble" begin
-            include("mirk/ensemble.jl")
-        end
-        @time @safetestset "MIRK Convergence Tests" begin
-            include("mirk/mirk_convergence_tests.jl")
-        end
-        @time @safetestset "Vector of Vector" begin
-            include("mirk/vectorofvector_initials.jl")
+    if GROUP == "ALL" || GROUP == "SHOOTING"
+        @time @testset "Shooting Method Tests" begin
+            @time @safetestset "Shooting Tests" begin
+                include("shooting/shooting_tests.jl")
+            end
+            @time @safetestset "Ray Tracing BVP" begin
+                include("shooting/ray_tracing.jl")
+            end
+            @time @safetestset "Orbital" begin
+                include("shooting/orbital.jl")
+            end
         end
     end
 
-    @time @testset "Miscelleneous" begin
-        @time @safetestset "Non Vector Inputs" begin
-            include("misc/non_vector_inputs.jl")
+    if GROUP == "ALL" || GROUP == "MIRK"
+        @time @testset "Collocation Method (MIRK) Tests" begin
+            @time @safetestset "Ensemble" begin
+                include("mirk/ensemble.jl")
+            end
+            @time @safetestset "MIRK Convergence Tests" begin
+                include("mirk/mirk_convergence_tests.jl")
+            end
+            @time @safetestset "Vector of Vector" begin
+                include("mirk/vectorofvector_initials.jl")
+            end
         end
+    end
 
-        @time @safetestset "Type Stability" begin
-            include("misc/type_stability.jl")
-        end
+    if GROUP == "ALL" || GROUP == "OTHERS"
+        @time @testset "Miscelleneous" begin
+            @time @safetestset "Non Vector Inputs" begin
+                include("misc/non_vector_inputs.jl")
+            end
 
-        @time @safetestset "ODE Interface Tests" begin
-            include("misc/odeinterface_ex7.jl")
+            @time @safetestset "Type Stability" begin
+                include("misc/type_stability.jl")
+            end
+
+            @time @safetestset "ODE Interface Tests" begin
+                include("misc/odeinterface_ex7.jl")
+            end
         end
     end
 end
