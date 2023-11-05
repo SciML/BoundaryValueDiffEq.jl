092e54435d88497039ae04755ba3c72e84059a5e
Avik Pal <avikpal@mit.edu>
2023-11-04 21:01:06 -0400

    Run a Benchmark CI for PRs for some baseline results

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,12 @@
+using BenchmarkTools
+using BoundaryValueDiffEq, OrdinaryDiffEq
+
+include("simple_pendulum.jl")
+
+function create_benchmark()
+    suite = BenchmarkGroup()
+    suite["Simple Pendulum"] = create_simple_pendulum_benchmark()
+    return suite
+end
+
+const SUITE = create_benchmark()
