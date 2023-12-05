2715132797820896583e47cb3aa8d4ec0d1c0c7e
Axel Larsson <65452706+axla-io@users.noreply.github.com>
2023-12-04 19:01:29 -0500

    Tested adaptivity for non nested

--- src/alg_utils.jl
+++ src/alg_utils.jl
@@ -22,6 +22,12 @@ for order in (2, 3, 4, 5)
     @eval alg_stage(::$(alg)) = $order
 end
 
+for order in (2, 3, 4, 5)
+    alg = Symbol("LobattoIIIc$(order)")
+    @eval alg_order(::$(alg)) = $order
+    @eval alg_stage(::$(alg)) = $order
+end
+
 SciMLBase.isautodifferentiable(::BoundaryValueDiffEqAlgorithm) = true
 SciMLBase.allows_arbitrary_number_types(::BoundaryValueDiffEqAlgorithm) = true
 SciMLBase.allowscomplex(alg::BoundaryValueDiffEqAlgorithm) = true
