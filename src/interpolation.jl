611205567b44740212ee94203eb62b3b33868844
Axel Larsson <65452706+axla-io@users.noreply.github.com>
2024-03-08 15:26:13 -0800

    Added interpolation

--- src/interpolation.jl
+++ src/interpolation.jl
@@ -60,3 +60,66 @@ end
     interp_eval!(z, id.cache, tval, id.cache.mesh, id.cache.mesh_dt)
     return idxs !== nothing ? z[idxs] : z
 end
+
+"""
+    get_ymid(yᵢ, coeffs, K, h)
+
+Gets the interpolated middle value for a RK method, see bvp5c paper.
+"""
+function get_ymid(yᵢ, coeffs, K, h)
+    res = copy(yᵢ)
+    for i in axes(K, 2)
+        res .+= h .* K[:, i] .* coeffs[i]
+    end
+    return res
+end
+
+"""
+    s_constraints(M, h)
+
+Form the quartic interpolation constraint matrix, see bvp5c paper.
+"""
+function s_constraints(M, h)
+    t = vec(repeat([0.0, 1.0*h, 0.5*h, 0.0, 1.0*h, 0.5*h], 1, M))
+    A = zeros(6 * M, 6 * M)
+    for i in 1:6
+        row_start = (i - 1) * M + 1
+        if i <= 3
+            for k = 0:M-1
+                for j in 1:6
+                    A[row_start+k, j+k*6] = t[i+k*6]^(j - 1)
+                end
+            end
+        else
+            for k = 0:M-1
+                for j in 1:6
+                    A[row_start+k, j+k*6] = j == 1.0 ? 0.0 : (j - 1) * t[i+k*6]^(j - 2)
+                end
+            end
+        end
+    end
+    return A
+end
+
+"""
+    get_s_coeffs(yᵢ, yᵢ₊₁, dyᵢ, dyᵢ₊₁, ymid)
+
+Gets the coefficients for the (local) s(x) polynomial, see bvp5c paper.
+"""
+function get_s_coeffs(yᵢ, yᵢ₊₁, dyᵢ, dyᵢ₊₁, ymid)
+    vals = vcat(yᵢ, yᵢ₊₁, dyᵢ, dyᵢ₊₁, ymid)
+    M = length(yᵢ)
+    A = s_constraints(M)
+    coeffs = reshape(A \ vals, 5, M)'
+    return coeffs
+end
+
+"""
+    s_interpolate(t, coeffs)
+
+Evaluate the s(x) interpolation, see bvp5c paper.
+"""
+function s_interpolate(t, coeffs)
+    ts = [t^(i - 1) for i in axes(coeffs, 2)]
+    return coeffs * ts
+end
