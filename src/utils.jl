cfdf8398ef5dfb7430f798849bf00efbc753e2bb
Axel Larsson <65452706+axla-io@users.noreply.github.com>
2024-03-08 15:33:39 -0800

    Updated utils

--- src/utils.jl
+++ src/utils.jl
@@ -123,6 +123,25 @@ function __append_similar!(x::AbstractVector{<:MaybeDiffCache}, n, M)
     return x
 end
 
+function __append_similar!(x::AbstractVector{<:AbstractArray}, n, _, TU::FIRKTableau{false})
+    @unpack s = TU
+    N = (n - 1) * (s + 1) + 1 - length(x)
+    N == 0 && return x
+    N < 0 && throw(ArgumentError("Cannot append a negative number of elements"))
+    append!(x, [similar(last(x)) for _ in 1:N])
+    return x
+end
+
+function __append_similar!(x::AbstractVector{<:MaybeDiffCache}, n, M, TU::FIRKTableau{false})
+    @unpack s = TU
+    N = (n - 1) * (s + 1) + 1 - length(x)
+    N == 0 && return x
+    N < 0 && throw(ArgumentError("Cannot append a negative number of elements"))
+    chunksize = isa(TU, FIRKTableau{false}) ? pickchunksize(M * (N + length(x) * (s + 1))) : pickchunksize(M * (N + length(x)))
+    append!(x, [__maybe_allocate_diffcache(last(x), chunksize) for _ in 1:N])
+    return x
+end
+
 ## Problem with Initial Guess
 function __extract_problem_details(prob; kwargs...)
     return __extract_problem_details(prob, prob.u0; kwargs...)
