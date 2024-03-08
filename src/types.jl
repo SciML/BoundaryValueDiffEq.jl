eb58a2a5a73e7e15c41dafdc1dea8aad82fedaeb
Axel Larsson <65452706+axla-io@users.noreply.github.com>
2024-03-08 15:31:34 -0800

    Added types

--- src/types.jl
+++ src/types.jl
@@ -28,6 +28,37 @@ struct MIRKInterpTableau{s, c, v, x, τ}
     end
 end
 
+@truncate_stacktrace MIRKInterpTableau 1
+
+# FIRK Method Tableaus
+struct FIRKTableau{nested, sType, aType, cType, bType}
+    """Discrete stages of RK formula"""
+    s::sType
+    a::aType
+    c::cType
+    b::bType
+
+    function FIRKTableau(s, a, c, b, nested)
+        @assert eltype(a) == eltype(c) == eltype(b)
+        return new{nested, typeof(s), typeof(a), typeof(c), typeof(b)}(s, a, c, b)
+    end
+end
+
+@truncate_stacktrace FIRKTableau 1
+
+struct FIRKInterpTableau{nested, c, m}
+    q_coeff::c
+    τ_star::m
+    stage::Int
+
+    function FIRKInterpTableau(q_coeff, τ_star, stage, nested::Bool)
+        @assert eltype(q_coeff) == eltype(τ_star)
+        return new{nested, typeof(q_coeff), typeof(τ_star)}(q_coeff, τ_star, stage)
+    end
+end
+
+@truncate_stacktrace FIRKInterpTableau 1
+
 # Sparsity Detection
 @concrete struct BVPJacobianAlgorithm
     bc_diffmode
