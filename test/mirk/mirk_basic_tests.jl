e058b0ffc1900823fda1eb6da44bf624097cf2d5
ErikQQY <2283984853@qq.com>
2024-07-13 16:18:52 +0800

    Fix CI
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- test/mirk/mirk_basic_tests.jl
+++ test/mirk/mirk_basic_tests.jl
@@ -95,9 +95,9 @@ end
         @testset "MIRK$order" for order in (2, 3, 4, 5, 6)
             solver = mirk_solver(Val(order); nlsolve = NewtonRaphson(),
                 jac_alg = BVPJacobianAlgorithm(AutoForwardDiff(; chunksize = 2)))
-            @test_opt target_modules=(NonlinearSolve, BoundaryValueDiffEq) solve(
+            @test_opt target_modules=(BoundaryValueDiffEq,) solve(
                 prob, solver; dt = 0.2)
-            @test_call target_modules=(NonlinearSolve, BoundaryValueDiffEq) solve(
+            @test_call target_modules=(BoundaryValueDiffEq,) solve(
                 prob, solver; dt = 0.2)
         end
     end
