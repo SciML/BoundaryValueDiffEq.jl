3f31f8946c43569b6c703487644d5d11bf582b55
ErikQQY <2283984853@qq.com>
2024-07-26 21:21:00 +0800

    Add tests for nonadaptive interpolations
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- test/mirk/mirk_basic_tests.jl
+++ test/mirk/mirk_basic_tests.jl
@@ -175,12 +175,20 @@ end
         @eval mirk_solver(::Val{$order}) = $(s)()
     end
 
-    @testset "MIRK$order" for order in (2, 3, 4, 5, 6)
+    @testset "Interpolation for adaptive MIRK$order" for order in (2, 3, 4, 5, 6)
         sol = solve(prob_bvp_linear, mirk_solver(Val(order)); dt = 0.001)
         @test sol(0.001)≈[0.998687464, -1.312035941] atol=testTol
         @test sol(0.001; idxs = [1, 2])≈[0.998687464, -1.312035941] atol=testTol
         @test sol(0.001; idxs = 1)≈0.998687464 atol=testTol
         @test sol(0.001; idxs = 2)≈-1.312035941 atol=testTol
+    end
+
+    @testset "Interpolation for non-adaptive MIRK$order" for order in (2, 3, 4, 5, 6)
+        sol = solve(prob_bvp_linear, mirk_solver(Val(order)); dt = 0.001, adaptive=false)
+        @test_nowarn sol(0.01)
+        @test_nowarn sol(0.01; idxs = [1, 2])
+        @test_nowarn sol(0.01; idxs = 1)
+        @test_nowarn sol(0.01; idxs = 2)
     end
 end
 
