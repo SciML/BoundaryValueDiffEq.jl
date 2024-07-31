8a251a9b830ea7b0969caa94843d86c0b08c634e
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-07-31 19:42:14 -0400

    Update nlls_tests.jl

--- test/mirk/nlls_tests.jl
+++ test/mirk/nlls_tests.jl
@@ -9,8 +9,8 @@
     f1(u, p, t) = [u[2], -u[1]]
 
     function bc1(sol, p, t)
-        solₜ₁ = sol.u[1]
-        solₜ₂ = sol.u[end]
+        solₜ₁ = sol[1]
+        solₜ₂ = sol[end]
         return [solₜ₁[1], solₜ₂[1] - 1, solₜ₂[2] + 1.729109]
     end
 
