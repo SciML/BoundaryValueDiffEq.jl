b92e1927cf4f1bab9a4670e4efd299ba35fe6671
ErikQQY <2283984853@qq.com>
2024-08-29 01:59:41 +0800

    Split into different test groups
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,34 @@
 using ReTestItems
 
-ReTestItems.runtests(joinpath(@__DIR__, "mirk/"))
-ReTestItems.runtests(joinpath(@__DIR__, "misc/"))
-ReTestItems.runtests(joinpath(@__DIR__, "shooting/"))
-ReTestItems.runtests(joinpath(@__DIR__, "firk/expanded/"))
-ReTestItems.runtests(joinpath(@__DIR__, "firk/nested/"))
+const GROUP = get(ENV, "GROUP", "All")
+const is_APPVEYOR = Sys.iswindows() && haskey(ENV, "APPVEYOR")
+
+@time begin
+    if GROUP == "All" || GROUP == "MRIK"
+        @time "MIRK solver" begin
+            ReTestItems.runtests(joinpath(@__DIR__, "mirk/"))
+        end
+    end
+
+    if GROUP == "All" || GROUP == "MISC"
+        @time "Miscellaneous" begin
+            ReTestItems.runtests(joinpath(@__DIR__, "misc/"))
+        end
+    end
+
+    if GROUP == "All" || GROUP == "SHOOTING"
+        @time "Shooting solver" begin
+            ReTestItems.runtests(joinpath(@__DIR__, "shooting/"))
+        end
+    end
+
+    if GROUP == "All" || GROUP == "FIRK"
+        @time "FIRK solver" begin
+            ReTestItems.runtests(joinpath(@__DIR__, "firk/expanded/"))
+            ReTestItems.runtests(joinpath(@__DIR__, "firk/nested/"))
+        end
+    end
+end
 
 if !Sys.iswindows() && !Sys.isapple()
     # Wrappers like ODEInterface don't support parallel testing
