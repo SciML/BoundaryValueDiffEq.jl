ca3fcc413c1bad92482b5fab10ddca6e00abaedb
ErikQQY <2283984853@qq.com>
2024-08-29 14:28:40 +0800

    Set Aqua tests as test_broken
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- test/misc/qa_tests.jl
+++ test/misc/qa_tests.jl
@@ -1,5 +1,6 @@
 @testitem "Quality Assurance" begin
     using Aqua
 
-    Aqua.test_all(BoundaryValueDiffEq; ambiguities = false)
+    @test_broken Aqua.test_all(BoundaryValueDiffEq; ambiguities = false)
+    @test_broken Aqua.test_ambiguities(BoundaryValueDiffEq; recursive = false)
 end
