0462e80fac53e787cf9a14bebeaa97152a82643a
Qingyu Qu <2283984853@qq.com>
2024-09-25 14:50:19 +0800

    refactor
    Signed-off-by: Qingyu Qu <2283984853@qq.com>

--- lib/BoundaryValueDiffEqFIRK/src/algorithms.jl
+++ lib/BoundaryValueDiffEqFIRK/src/algorithms.jl
@@ -39,44 +39,46 @@ for stage in (1, 2, 3, 5, 7)
           `BVPJacobianAlgorithm` must be provided.
 
       ## References
-            Reference for Lobatto and Radau methods:
-
-                @incollection{Jay2015,
-                author="Jay, Laurent O.",
-                editor="Engquist, Bj{\"o}rn",
-                title="Lobatto Methods",
-                booktitle = {Encyclopedia of {Applied} and {Computational} {Mathematics}},
-                year="2015",
-                publisher="Springer Berlin Heidelberg",
-                }
-                @incollection{engquist_radau_2015,
-                author = {Hairer, Ernst and Wanner, Gerhard},
-                editor={Engquist, Bj{\"o}rn},
-                title = {Radau {Methods}},
-                booktitle = {Encyclopedia of {Applied} and {Computational} {Mathematics}},
-                publisher = {Springer Berlin Heidelberg},
-                year = {2015},
-            }
-      References for implementation of defect control, based on the `bvp5c` solver in MATLAB:
-
+        Reference for Lobatto and Radau methods:
+        ```bibtex
+        @incollection{Jay2015,
+            author="Jay, Laurent O.",
+            editor="Engquist, Bj{\"o}rn",
+            title="Lobatto Methods",
+            booktitle = {Encyclopedia of {Applied} and {Computational} {Mathematics}},
+            year="2015",
+            publisher="Springer Berlin Heidelberg",
+        }
+        @incollection{engquist_radau_2015,
+            author = {Hairer, Ernst and Wanner, Gerhard},
+            editor={Engquist, Bj{\"o}rn},
+            title = {Radau {Methods}},
+            booktitle = {Encyclopedia of {Applied} and {Computational} {Mathematics}},
+            publisher = {Springer Berlin Heidelberg},
+            year = {2015},
+        }
+        ```
+        References for implementation of defect control, based on the `bvp5c` solver in MATLAB:
+        ```bibtex
         @article{shampine_solving_nodate,
-        title = {Solving {Boundary} {Value} {Problems} for {Ordinary} {Diﬀerential} {Equations} in {Matlab} with bvp4c
-        author = {Shampine, Lawrence F and Kierzenka, Jacek and Reichelt, Mark W},
-        year = {2000},
-    }
+            title = {Solving {Boundary} {Value} {Problems} for {Ordinary} {Diﬀerential} {Equations} in {Matlab} with bvp4c
+            author = {Shampine, Lawrence F and Kierzenka, Jacek and Reichelt, Mark W},
+            year = {2000},
+        }
 
-    @article{kierzenka_bvp_2008,
-        title = {A {BVP} {Solver} that {Controls} {Residual} and {Error}},
-        author = {Kierzenka, J and Shampine, L F},
-        year = {2008},
-    }
+        @article{kierzenka_bvp_2008,
+            title = {A {BVP} {Solver} that {Controls} {Residual} and {Error}},
+            author = {Kierzenka, J and Shampine, L F},
+            year = {2008},
+        }
 
-    @article{russell_adaptive_1978,
-        title = {Adaptive {Mesh} {Selection} {Strategies} for {Solving} {Boundary} {Value} {Problems}},
-        journal = {SIAM Journal on Numerical Analysis},
-        author = {Russell, R. D. and Christiansen, J.},
-        year = {1978},
-    }
+        @article{russell_adaptive_1978,
+            title = {Adaptive {Mesh} {Selection} {Strategies} for {Solving} {Boundary} {Value} {Problems}},
+            journal = {SIAM Journal on Numerical Analysis},
+            author = {Russell, R. D. and Christiansen, J.},
+            year = {1978},
+        }
+        ```
         """
         Base.@kwdef struct $(alg){N, J <: BVPJacobianAlgorithm, T} <: AbstractFIRK
             nlsolve::N = nothing
@@ -104,9 +106,9 @@ for stage in (2, 3, 4, 5)
 
         ## Keyword Arguments
 
-        - `nlsolve`: Internal Nonlinear solver. Any solver which conforms to the SciML
+          - `nlsolve`: Internal Nonlinear solver. Any solver which conforms to the SciML
           `NonlinearProblem` interface can be used. Note that any autodiff argument for
-          the solver will be ignored and a custom jacobian algorithm will be used.
+            the solver will be ignored and a custom jacobian algorithm will be used.
         - `jac_alg`: Jacobian Algorithm used for the nonlinear solver. Defaults to
           `BVPJacobianAlgorithm()`, which automatically decides the best algorithm to
           use based on the input types and problem type.
@@ -117,7 +119,7 @@ for stage in (2, 3, 4, 5)
             `AutoSparse(AutoFiniteDiff)`. For `bc_diffmode`, defaults to `AutoForwardDiff` if
             possible else `AutoFiniteDiff`.
         - `nested_nlsolve`: Whether or not to use a nested nonlinear solve for the 
-        implicit FIRK step. Defaults to `false`. If set to `false`, the FIRK stages are 
+            implicit FIRK step. Defaults to `false`. If set to `false`, the FIRK stages are 
         solved as a part of the global residual. The general recommendation is to choose 
         `true` for larger problems and `false` for smaller ones.
         - `nest_tol`: The tolerance for the nested solver. Default is nothing which leads to 
@@ -131,7 +133,7 @@ for stage in (2, 3, 4, 5)
 
       ## References
             Reference for Lobatto and Radau methods:
-
+            ```bibtex
                 @Inbook{Jay2015,
                 author="Jay, Laurent O.",
                 editor="Engquist, Bj{\"o}rn",
@@ -148,27 +150,29 @@ for stage in (2, 3, 4, 5)
                 editor="Engquist, Bj{\"o}rn",
                 year = {2015},
             }
-      References for implementation of defect control, based on the `bvp5c` solver in MATLAB:
+            ```
+            References for implementation of defect control, based on the `bvp5c` solver in MATLAB:
+            ```bibtex
+            @article{shampine_solving_nodate,
+            title = {Solving {Boundary} {Value} {Problems} for {Ordinary} {Diﬀerential} {Equations} in {Matlab} with bvp4c
+            author = {Shampine, Lawrence F and Kierzenka, Jacek and Reichelt, Mark W},
+            year = {2000},
+            }
 
-        @article{shampine_solving_nodate,
-        title = {Solving {Boundary} {Value} {Problems} for {Ordinary} {Diﬀerential} {Equations} in {Matlab} with bvp4c
-        author = {Shampine, Lawrence F and Kierzenka, Jacek and Reichelt, Mark W},
-        year = {2000},
-    }
+            @article{kierzenka_bvp_2008,
+                title = {A {BVP} {Solver} that {Controls} {Residual} and {Error}},
+                author = {Kierzenka, J and Shampine, L F},
+                year = {2008},
+            }
 
-    @article{kierzenka_bvp_2008,
-        title = {A {BVP} {Solver} that {Controls} {Residual} and {Error}},
-        author = {Kierzenka, J and Shampine, L F},
-        year = {2008},
-    }
-
-    @article{russell_adaptive_1978,
-        title = {Adaptive {Mesh} {Selection} {Strategies} for {Solving} {Boundary} {Value} {Problems}},
-        journal = {SIAM Journal on Numerical Analysis},
-        author = {Russell, R. D. and Christiansen, J.},
-        year = {1978},
-        file = {Russell and Christiansen - 1978 - Adaptive Mesh Selection Strategies for Solving Bou.pdf:/Users/AXLRSN/Zotero/storage/HKU27A4T/Russell and Christiansen - 1978 - Adaptive Mesh Selection Strategies for Solving Bou.pdf:application/pdf},
-    }
+            @article{russell_adaptive_1978,
+                title = {Adaptive {Mesh} {Selection} {Strategies} for {Solving} {Boundary} {Value} {Problems}},
+                journal = {SIAM Journal on Numerical Analysis},
+                author = {Russell, R. D. and Christiansen, J.},
+                year = {1978},
+                file = {Russell and Christiansen - 1978 - Adaptive Mesh Selection Strategies for Solving Bou.pdf:/Users/AXLRSN/Zotero/storage/HKU27A4T/Russell and Christiansen - 1978 - Adaptive Mesh Selection Strategies for Solving Bou.pdf:application/pdf},
+            }
+            ```
         """
         Base.@kwdef struct $(alg){N, J <: BVPJacobianAlgorithm, T} <: AbstractFIRK
             nlsolve::N = nothing
@@ -222,45 +226,47 @@ for stage in (2, 3, 4, 5)
           `BVPJacobianAlgorithm` must be provided.
 
       ## References
-            Reference for Lobatto and Radau methods:
-
-                @Inbook{Jay2015,
-                author="Jay, Laurent O.",
-                editor="Engquist, Bj{\"o}rn",
-                title="Lobatto Methods",
-                booktitle = {Encyclopedia of {Applied} and {Computational} {Mathematics}},
-                year="2015",
-                publisher="Springer Berlin Heidelberg",
-                }
-                @incollection{engquist_radau_2015,
-                author = {Hairer, Ernst and Wanner, Gerhard},
-                title = {Radau {Methods}},
-                booktitle = {Encyclopedia of {Applied} and {Computational} {Mathematics}},
-                publisher = {Springer Berlin Heidelberg},
-                editor="Engquist, Bj{\"o}rn",
-                year = {2015},
-            }
-      References for implementation of defect control, based on the `bvp5c` solver in MATLAB:
-
+        Reference for Lobatto and Radau methods:
+        ```bibtex
+        @Inbook{Jay2015,
+            author="Jay, Laurent O.",
+            editor="Engquist, Bj{\"o}rn",
+            title="Lobatto Methods",
+            booktitle = {Encyclopedia of {Applied} and {Computational} {Mathematics}},
+            year="2015",
+            publisher="Springer Berlin Heidelberg",
+        }
+        @incollection{engquist_radau_2015,
+            author = {Hairer, Ernst and Wanner, Gerhard},
+            title = {Radau {Methods}},
+            booktitle = {Encyclopedia of {Applied} and {Computational} {Mathematics}},
+            publisher = {Springer Berlin Heidelberg},
+            editor="Engquist, Bj{\"o}rn",
+            year = {2015},
+        }
+        ```
+        References for implementation of defect control, based on the `bvp5c` solver in MATLAB:
+        ```bibtex
         @article{shampine_solving_nodate,
-        title = {Solving {Boundary} {Value} {Problems} for {Ordinary} {Diﬀerential} {Equations} in {Matlab} with bvp4c
-        author = {Shampine, Lawrence F and Kierzenka, Jacek and Reichelt, Mark W},
-        year = {2000},
-    }
+            title = {Solving {Boundary} {Value} {Problems} for {Ordinary} {Diﬀerential} {Equations} in {Matlab} with bvp4c
+            author = {Shampine, Lawrence F and Kierzenka, Jacek and Reichelt, Mark W},
+            year = {2000},
+        }
 
-    @article{kierzenka_bvp_2008,
-        title = {A {BVP} {Solver} that {Controls} {Residual} and {Error}},
-        author = {Kierzenka, J and Shampine, L F},
-        year = {2008},
-    }
+        @article{kierzenka_bvp_2008,
+            title = {A {BVP} {Solver} that {Controls} {Residual} and {Error}},
+            author = {Kierzenka, J and Shampine, L F},
+            year = {2008},
+        }
 
-    @article{russell_adaptive_1978,
-        title = {Adaptive {Mesh} {Selection} {Strategies} for {Solving} {Boundary} {Value} {Problems}},
-        journal = {SIAM Journal on Numerical Analysis},
-        author = {Russell, R. D. and Christiansen, J.},
-        year = {1978},
-        file = {Russell and Christiansen - 1978 - Adaptive Mesh Selection Strategies for Solving Bou.pdf:/Users/AXLRSN/Zotero/storage/HKU27A4T/Russell and Christiansen - 1978 - Adaptive Mesh Selection Strategies for Solving Bou.pdf:application/pdf},
-    }
+        @article{russell_adaptive_1978,
+            title = {Adaptive {Mesh} {Selection} {Strategies} for {Solving} {Boundary} {Value} {Problems}},
+            journal = {SIAM Journal on Numerical Analysis},
+            author = {Russell, R. D. and Christiansen, J.},
+            year = {1978},
+            file = {Russell and Christiansen - 1978 - Adaptive Mesh Selection Strategies for Solving Bou.pdf:/Users/AXLRSN/Zotero/storage/HKU27A4T/Russell and Christiansen - 1978 - Adaptive Mesh Selection Strategies for Solving Bou.pdf:application/pdf},
+        }
+        ```
         """
         Base.@kwdef struct $(alg){N, J <: BVPJacobianAlgorithm, T} <: AbstractFIRK
             nlsolve::N = nothing
@@ -314,45 +320,47 @@ for stage in (2, 3, 4, 5)
           `BVPJacobianAlgorithm` must be provided.
 
       ## References
-            Reference for Lobatto and Radau methods:
-
-                @Inbook{Jay2015,
-                author="Jay, Laurent O.",
-                editor="Engquist, Bj{\"o}rn",
-                title="Lobatto Methods",
-                booktitle = {Encyclopedia of {Applied} and {Computational} {Mathematics}},
-                year="2015",
-                publisher="Springer Berlin Heidelberg",
-                }
-                @incollection{engquist_radau_2015,
-                author = {Hairer, Ernst and Wanner, Gerhard},
-                title = {Radau {Methods}},
-                booktitle = {Encyclopedia of {Applied} and {Computational} {Mathematics}},
-                publisher = {Springer Berlin Heidelberg},
-                editor="Engquist, Bj{\"o}rn",
-                year = {2015},
-            }
+        Reference for Lobatto and Radau methods:
+        ```bibtex
+        @Inbook{Jay2015,
+            author="Jay, Laurent O.",
+            editor="Engquist, Bj{\"o}rn",
+            title="Lobatto Methods",
+            booktitle = {Encyclopedia of {Applied} and {Computational} {Mathematics}},
+            year="2015",
+            publisher="Springer Berlin Heidelberg",
+        }
+        @incollection{engquist_radau_2015,
+            author = {Hairer, Ernst and Wanner, Gerhard},
+            title = {Radau {Methods}},
+            booktitle = {Encyclopedia of {Applied} and {Computational} {Mathematics}},
+            publisher = {Springer Berlin Heidelberg},
+            editor="Engquist, Bj{\"o}rn",
+            year = {2015},
+        }
+        ```
       References for implementation of defect control, based on the `bvp5c` solver in MATLAB:
-
+        ```bibtex
         @article{shampine_solving_nodate,
         title = {Solving {Boundary} {Value} {Problems} for {Ordinary} {Diﬀerential} {Equations} in {Matlab} with bvp4c
         author = {Shampine, Lawrence F and Kierzenka, Jacek and Reichelt, Mark W},
         year = {2000},
-    }
+        }
 
-    @article{kierzenka_bvp_2008,
-        title = {A {BVP} {Solver} that {Controls} {Residual} and {Error}},
-        author = {Kierzenka, J and Shampine, L F},
-        year = {2008},
-    }
+        @article{kierzenka_bvp_2008,
+            title = {A {BVP} {Solver} that {Controls} {Residual} and {Error}},
+            author = {Kierzenka, J and Shampine, L F},
+            year = {2008},
+        }
 
-    @article{russell_adaptive_1978,
-        title = {Adaptive {Mesh} {Selection} {Strategies} for {Solving} {Boundary} {Value} {Problems}},
-        journal = {SIAM Journal on Numerical Analysis},
-        author = {Russell, R. D. and Christiansen, J.},
-        year = {1978},
-        file = {Russell and Christiansen - 1978 - Adaptive Mesh Selection Strategies for Solving Bou.pdf:/Users/AXLRSN/Zotero/storage/HKU27A4T/Russell and Christiansen - 1978 - Adaptive Mesh Selection Strategies for Solving Bou.pdf:application/pdf},
-    }
+        @article{russell_adaptive_1978,
+            title = {Adaptive {Mesh} {Selection} {Strategies} for {Solving} {Boundary} {Value} {Problems}},
+            journal = {SIAM Journal on Numerical Analysis},
+            author = {Russell, R. D. and Christiansen, J.},
+            year = {1978},
+            file = {Russell and Christiansen - 1978 - Adaptive Mesh Selection Strategies for Solving Bou.pdf:/Users/AXLRSN/Zotero/storage/HKU27A4T/Russell and Christiansen - 1978 - Adaptive Mesh Selection Strategies for Solving Bou.pdf:application/pdf},
+        }
+        ```
         """
         Base.@kwdef struct $(alg){N, J <: BVPJacobianAlgorithm, T} <: AbstractFIRK
             nlsolve::N = nothing
