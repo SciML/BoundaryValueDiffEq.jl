711a6b33114fead73d7748370b5a8de6c775b524
Qingyu Qu <2283984853@qq.com>
2024-11-09 16:49:55 +0800

    Fix FIRK CI

--- lib/BoundaryValueDiffEqFIRK/test/runtests.jl
+++ lib/BoundaryValueDiffEqFIRK/test/runtests.jl
@@ -1,9 +1,19 @@
-using ReTestItems, BoundaryValueDiffEqFIRK
+using ReTestItems, BoundaryValueDiffEqFIRK, Hwloc, InteractiveUtils
 
-@time "FIRK Expanded solvers" begin
-    ReTestItems.runtests("expanded/")
-end
+@info sprint(InteractiveUtils.versioninfo)
 
-@time "FIRK Nested solvers" begin
-    ReTestItems.runtests("nested/")
-end
+const GROUP = lowercase(get(ENV, "GROUP", "All"))
+
+const RETESTITEMS_NWORKERS = parse(Int,
+    get(ENV, "RETESTITEMS_NWORKERS",
+        string(min(ifelse(Sys.iswindows(), 0, Hwloc.num_physical_cores()), 4))))
+const RETESTITEMS_NWORKER_THREADS = parse(Int,
+    get(ENV, "RETESTITEMS_NWORKER_THREADS",
+        string(max(Hwloc.num_virtual_cores() ÷ max(RETESTITEMS_NWORKERS, 1), 1))))
+
+@info "Running tests for group: $(GROUP) with $(RETESTITEMS_NWORKERS) workers"
+
+ReTestItems.runtests(
+    BoundaryValueDiffEqFIRK; tags = (GROUP == "all" ? nothing : [Symbol(GROUP)]),
+    nworkers = RETESTITEMS_NWORKERS,
+    nworker_threads = RETESTITEMS_NWORKER_THREADS, testitem_timeout = 300 * 60)
