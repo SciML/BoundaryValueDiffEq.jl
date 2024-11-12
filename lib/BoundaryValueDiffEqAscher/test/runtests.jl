4997ba600f4c5054453886c1409f6cb7f0a188e2
Qingyu Qu <2283984853@qq.com>
2024-11-12 16:01:02 +0800

    Fix append_similar! for FIRK nested methods

--- lib/BoundaryValueDiffEqAscher/test/runtests.jl
+++ lib/BoundaryValueDiffEqAscher/test/runtests.jl
@@ -15,4 +15,5 @@ const RETESTITEMS_NWORKER_THREADS = parse(Int,
 
 ReTestItems.runtests(
     BoundaryValueDiffEqAscher; tags = (GROUP == "all" ? nothing : [Symbol(GROUP)]),
-    nworkers = RETESTITEMS_NWORKERS, nworker_threads = RETESTITEMS_NWORKER_THREADS)
+    nworkers = RETESTITEMS_NWORKERS,
+    nworker_threads = RETESTITEMS_NWORKER_THREADS, testitem_timeout = 3 * 60 * 60)
