9f18a3b89ed9c655879d664e82124e013fd75121
Qingyu Qu <2283984853@qq.com>
2024-12-07 23:20:02 +0800

    Boundary conditions should always use solution object

--- lib/BoundaryValueDiffEqMIRK/src/interpolation.jl
+++ lib/BoundaryValueDiffEqMIRK/src/interpolation.jl
@@ -21,6 +21,7 @@ end
 @inline function interpolation(tvals, id::MIRKInterpolation, idxs, deriv::D,
         p, continuity::Symbol = :left) where {D}
     (; t, u, cache) = id
+    (; mesh, mesh_dt) = cache
     tdir = sign(t[end] - t[1])
     idx = sortperm(tvals, rev = tdir < 0)
 
@@ -34,7 +35,7 @@ end
 
     for j in idx
         z = similar(cache.fᵢ₂_cache)
-        interpolant!(z, id.cache, tvals[j], id.cache.mesh, id.cache.mesh_dt, deriv)
+        interpolant!(z, id, cache, tvals[j], mesh, mesh_dt, deriv)
         vals[j] = idxs !== nothing ? z[idxs] : z
     end
     return DiffEqArray(vals, tvals)
@@ -43,41 +44,89 @@ end
 @inline function interpolation!(vals, tvals, id::MIRKInterpolation, idxs,
         deriv::D, p, continuity::Symbol = :left) where {D}
     (; t, cache) = id
+    (; mesh, mesh_dt) = cache
     tdir = sign(t[end] - t[1])
     idx = sortperm(tvals, rev = tdir < 0)
 
     for j in idx
-        z = similar(cache.fᵢ₂_cache)
-        interpolant!(z, id.cache, tvals[j], id.cache.mesh, id.cache.mesh_dt, deriv)
+        z = similar(id.u[1])
+        interpolant!(z, id, cache, tvals[j], mesh, mesh_dt, deriv)
         vals[j] = z
     end
 end
 
 @inline function interpolation(tval::Number, id::MIRKInterpolation, idxs,
         deriv::D, p, continuity::Symbol = :left) where {D}
-    z = similar(id.cache.fᵢ₂_cache)
-    interpolant!(z, id.cache, tval, id.cache.mesh, id.cache.mesh_dt, deriv)
+    z = similar(id.u[1])
+    interpolant!(z, id, id.cache, tval, id.cache.mesh, id.cache.mesh_dt, deriv)
     return idxs !== nothing ? z[idxs] : z
 end
 
 @inline function interpolant!(
-        z::AbstractArray, cache::MIRKCache, t, mesh, mesh_dt, T::Type{Val{0}})
+        z::AbstractArray, id, cache::MIRKCache, t, mesh, mesh_dt, T::Type{Val{0}})
     i = interval(mesh, t)
     dt = mesh_dt[i]
     τ = (t - mesh[i]) / dt
     w, w′ = interp_weights(τ, cache.alg)
-    sum_stages!(z, cache, w, i)
+    sum_stages!(z, id, cache, w, i)
 end
 
-@inline function interpolant!(
-        dz::AbstractArray, cache::MIRKCache, t, mesh, mesh_dt, T::Type{Val{1}})
+@inline function interpolant!(dz::AbstractArray, id::MIRKInterpolation,
+        cache::MIRKCache, t, mesh, mesh_dt, T::Type{Val{1}})
     i = interval(mesh, t)
     dt = mesh_dt[i]
     τ = (t - mesh[i]) / dt
     w, w′ = interp_weights(τ, cache.alg)
     z = similar(dz)
-    sum_stages!(z, dz, cache, w, w′, i)
+    sum_stages!(z, dz, id, cache, w, w′, i)
+end
+
+function sum_stages!(z::AbstractArray, id::MIRKInterpolation,
+        cache::MIRKCache, w, i::Int, dt = cache.mesh_dt[i])
+    (; stage, k_discrete, k_interp) = cache
+    (; s_star) = cache.ITU
+    z .= zero(z)
+    __maybe_matmul!(z, k_discrete[i].du[:, 1:stage], w[1:stage])
+    __maybe_matmul!(
+        z, k_interp.u[i][:, 1:(s_star - stage)], w[(stage + 1):s_star], true, true)
+    z .= z .* dt .+ id.u[i]
+
+    return z
+end
+
+@views function sum_stages!(z, z′, id::MIRKInterpolation, cache::MIRKCache,
+        w, w′, i::Int, dt = cache.mesh_dt[i])
+    (; stage, k_discrete, k_interp) = cache
+    (; s_star) = cache.ITU
+
+    z .= zero(z)
+    __maybe_matmul!(z, k_discrete[i].du[:, 1:stage], w[1:stage])
+    __maybe_matmul!(
+        z, k_interp.u[i][:, 1:(s_star - stage)], w[(stage + 1):s_star], true, true)
+    z′ .= zero(z′)
+    __maybe_matmul!(z′, k_discrete[i].du[:, 1:stage], w′[1:stage])
+    __maybe_matmul!(
+        z′, k_interp.u[i][:, 1:(s_star - stage)], w′[(stage + 1):s_star], true, true)
+    z .= z .* dt[1] .+ id.u[i]
+
+    return z, z′
 end
 
 @inline __build_interpolation(cache::MIRKCache, u::AbstractVector) = MIRKInterpolation(
     cache.mesh, u, cache)
+
+# Intermidiate solution for evaluating boundry conditions
+# basically simplified version of the interpolation for MIRK
+function (s::EvalSol{A})(tval::Number) where {A <: AbstractMIRK}
+    (; u, t, alg, k_discrete) = s
+    stage = alg_stage(alg)
+    z = similar(u[1])
+    i = interval(t, tval)
+    dt = t[i + 1] - t[i]
+    τ = (tval - t[i]) / dt
+    w, _ = interp_weights(τ, alg)
+    z .= zero(z)
+    __maybe_matmul!(z, k_discrete[i].du[:, 1:stage], w[1:stage])
+    z .= z .* dt .+ u[i]
+    return z
+end
