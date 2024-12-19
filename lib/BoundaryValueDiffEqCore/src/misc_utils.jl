eb1350050029fdf407e38be91cc084429f565c91
Qingyu Qu <2283984853@qq.com>
2024-12-19 17:57:51 +0800

    Also support array indexing in bc

--- lib/BoundaryValueDiffEqCore/src/misc_utils.jl
+++ lib/BoundaryValueDiffEqCore/src/misc_utils.jl
@@ -4,3 +4,11 @@
     t
     cache::C
 end
+
+Base.size(e::EvalSol) = (size(e.u[1])..., length(e.u))
+Base.size(e::EvalSol, i) = size(e)[i]
+
+Base.axes(e::EvalSol) = Base.OneTo.(size(e))
+Base.axes(e::EvalSol, i) = Base.OneTo.(size(e)[d])
+
+Base.getindex(e::EvalSol, args...) = Base.getindex(VectorOfArray(e.u), args...)
