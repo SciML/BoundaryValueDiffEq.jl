31da7f3cae750eb1b66d476f436dd384a370233c
Qingyu Qu <2283984853@qq.com>
2025-01-17 03:01:31 +0800

    Add more utils for EvalSol

--- lib/BoundaryValueDiffEqCore/src/misc_utils.jl
+++ lib/BoundaryValueDiffEqCore/src/misc_utils.jl
@@ -5,6 +5,8 @@
     cache::C
 end
 
+# Basic utilities for EvalSol
+
 Base.size(e::EvalSol) = (size(e.u[1])..., length(e.u))
 Base.size(e::EvalSol, i) = size(e)[i]
 
@@ -12,3 +14,18 @@ Base.axes(e::EvalSol) = Base.OneTo.(size(e))
 Base.axes(e::EvalSol, d::Int) = Base.OneTo.(size(e)[d])
 
 Base.getindex(e::EvalSol, args...) = Base.getindex(VectorOfArray(e.u), args...)
+Base.eachindex(e::EvalSol) = Base.eachindex(e.u)
+
+Base.first(e::EvalSol) = first(e.u)
+Base.last(e::EvalSol) = last(e.u)
+
+Base.firstindex(e::EvalSol) = 1
+Base.lastindex(e::EvalSol) = length(e.u)
+
+Base.length(e::EvalSol) = length(e.u)
+
+Base.eltype(e::EvalSol) = eltype(e.u)
+
+function Base.show(io::IO, m::MIME"text/plain", e::EvalSol)
+    (print(io, "t: "); show(io, m, e.t); println(io); print(io, "u: "); show(io, m, e.u))
+end
