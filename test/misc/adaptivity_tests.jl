4a782ffb515afc82177759ddf379a7e0e75037ad
Qingyu Qu <2283984853@qq.com>
2025-03-06 20:24:28 +0800

    Better tests

--- test/misc/adaptivity_tests.jl
+++ test/misc/adaptivity_tests.jl
@@ -6,12 +6,13 @@
         du[2] = -t / p * u[2] - pi^2 * cos(pi * t) - pi * t / p * sin(pi * t)
     end
     function bc!(res, u, p, t)
-        res[1] = u[1][1] + 2
-        res[2] = u[end][1]
+        res[1] = u[:, 1][1] + 2
+        res[2] = u[:, end][1]
     end
     tspan = (-1.0, 1.0)
     sol = [1.0, 0.0]
     prob = BVProblem(f!, bc!, sol, tspan, 0.001)
+
     for order in (2, 3, 4, 5, 6)
         s = Symbol("MIRK$(order)")
         @eval mirk_solver(::Val{$order}) = $(s)()
@@ -38,23 +39,23 @@
         s = Symbol("RadauIIa$(stage)")
         @eval radau_solver(::Val{$stage}, args...; kwargs...) = $(s)(args...; kwargs...)
     end
-    @testset "MIRK methods" for order in (2, 3, 4, 5, 6)
+    @testset "MIRK$order method" for order in (2, 3, 4, 5, 6)
         @test_nowarn sol = solve(prob, mirk_solver(Val(order)), dt = 0.01)
     end
 
-    @testset "RadauIIa methods" for stage in (2, 3, 5, 7)
+    @testset "RadauIIa$stage method" for stage in (2, 3, 5, 7)
         @test_nowarn sol = solve(prob, radau_solver(Val(stage)), dt = 0.01)
     end
 
-    @testset "LobattoIIIa methods" for stage in (2, 3, 4, 5)
+    @testset "LobattoIIIa$stage method" for stage in (2, 3, 4, 5)
         @test_nowarn sol = solve(prob, lobattoIIIa_solver(Val(stage)), dt = 0.01)
     end
 
-    @testset "LobattoIIIb methods" for stage in (3, 4, 5)
+    @testset "LobattoIIIb$stage method" for stage in (3, 4, 5)
         @test_nowarn sol = solve(prob, lobattoIIIb_solver(Val(stage)), dt = 0.01)
     end
 
-    @testset "LobattoIIIc methods" for stage in (3, 4, 5)
+    @testset "LobattoIIIc$stage method" for stage in (3, 4, 5)
         @test_nowarn sol = solve(prob, lobattoIIIc_solver(Val(stage)), dt = 0.01)
     end
 end
