9127dec3d7ba92d57ee7e4a8b7aa48ab1ebbb147
Qingyu Qu <2283984853@qq.com>
2025-04-15 14:51:54 +0800

    Fix typos

--- lib/BoundaryValueDiffEqFIRK/src/BoundaryValueDiffEqFIRK.jl
+++ lib/BoundaryValueDiffEqFIRK/src/BoundaryValueDiffEqFIRK.jl
@@ -5,16 +5,19 @@ using ArrayInterface: fast_scalar_indexing
 using BandedMatrices: BandedMatrix, Ones
 using BoundaryValueDiffEqCore: AbstractBoundaryValueDiffEqAlgorithm,
                                AbstractBoundaryValueDiffEqCache, BVPJacobianAlgorithm,
-                               get_abstol
-recursive_flatten,
-recursive_flatten!, recursive_unflatten!, __concrete_nonlinearsolve_algorithm,
-diff!, EvalSol, concrete_jacobian_algorithm, eval_bc_residual, eval_bc_residual!,
-get_tmp, __maybe_matmul!, __resize!, __extract_problem_details, __initial_guess,
-__default_coloring_algorithm, __maybe_allocate_diffcache, __restructure_sol,
-__get_bcresid_prototype, safe_similar, __vec, __vec_f, __vec_f!, __vec_bc, __vec_bc!,
-recursive_flatten_twopoint!, __internal_nlsolve_problem, MaybeDiffCache, __extract_mesh,
-__extract_u0, __has_initial_guess, __initial_guess_length, __initial_guess_on_mesh,
-__flatten_initial_guess, __build_solution, __Fix3, _sparse_like, get_dense_ad
+                               get_abstol, recursive_flatten, recursive_flatten!,
+                               recursive_unflatten!, __concrete_nonlinearsolve_algorithm,
+                               diff!, EvalSol, concrete_jacobian_algorithm,
+                               eval_bc_residual, eval_bc_residual!, get_tmp,
+                               __maybe_matmul!, __resize!, __extract_problem_details,
+                               __initial_guess, __default_coloring_algorithm,
+                               __maybe_allocate_diffcache, __restructure_sol,
+                               __get_bcresid_prototype, safe_similar, __vec, __vec_f,
+                               __vec_f!, __vec_bc, __vec_bc!, recursive_flatten_twopoint!,
+                               __internal_nlsolve_problem, MaybeDiffCache, __extract_mesh,
+                               __extract_u0, __has_initial_guess, __initial_guess_length,
+                               __initial_guess_on_mesh, __flatten_initial_guess,
+                               __build_solution, __Fix3, _sparse_like, get_dense_ad
 
 using ConcreteStructs: @concrete
 using DiffEqBase: DiffEqBase
