3418a5bbd5ccf09698d0adbdde95651d9d440f0e
Qingyu Qu <2283984853@qq.com>
2025-06-15 04:05:25 +0800

    Set testing atol

--- lib/BoundaryValueDiffEqMIRK/test/mirk_basic_tests.jl
+++ lib/BoundaryValueDiffEqMIRK/test/mirk_basic_tests.jl
@@ -441,7 +441,7 @@ end
         bcresid_prototype = (zeros(2), zeros(1)), fit_parameters = true)
     sol = solve(bvp, MIRK4(), dt = 0.05)
 
-    @test sol.prob.p ≈ [17.096580050]
+    @test sol.prob.p ≈ [17.09658] atol=1e-6
 
     tspan = (0.0, pi)
     function f!(du, u, p, t)
@@ -460,5 +460,5 @@ end
         bcresid_prototype = (zeros(2), zeros(1)), fit_parameters = true)
     sol = solve(bvp, MIRK4(), dt = 0.05)
 
-    @test sol.prob.p ≈ [17.09658]
+    @test sol.prob.p ≈ [17.09658] atol=1e-6
 end
