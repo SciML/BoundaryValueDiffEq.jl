81da921f48d4e7d29eaa05444d1dcdbf083a9222
Qingyu Qu <2283984853@qq.com>
2025-07-22 22:47:30 +0800

    Use correct setup in FIRK testing

--- lib/BoundaryValueDiffEqFIRK/test/expanded/firk_basic_tests.jl
+++ lib/BoundaryValueDiffEqFIRK/test/expanded/firk_basic_tests.jl
@@ -273,7 +273,7 @@ end
     @test_nowarn solve(bvp1, RadauIIa7(nl_solve, jac_alg; nested); dt = 0.05)
 end
 
-@testitem "Interpolation" begin
+@testitem "Interpolation" setup=[FIRKExpandedConvergenceTests] begin
     using LinearAlgebra
 
     λ = 1
