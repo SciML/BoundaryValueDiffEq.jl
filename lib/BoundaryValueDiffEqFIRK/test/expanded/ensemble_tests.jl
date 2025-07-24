44d556f8568d7f3773243e666280fc7277dcc008
Qingyu Qu <2283984853@qq.com>
2025-07-24 16:04:25 +0800

    Fix more FIRK tests

--- lib/BoundaryValueDiffEqFIRK/test/expanded/ensemble_tests.jl
+++ lib/BoundaryValueDiffEqFIRK/test/expanded/ensemble_tests.jl
@@ -40,7 +40,7 @@
                 AutoSparse(AutoFiniteDiff()); bc_diffmode = AutoFiniteDiff(),
                 nonbc_diffmode = AutoSparse(AutoFiniteDiff()))]
         for jac_alg in jac_algs
-            sol = solve(ensemble_prob, solver(; nlsolve, jac_alg; nested_nlsolve = nested);
+            sol = solve(ensemble_prob, solver(; nlsolve, jac_alg, nested_nlsolve = nested);
                 trajectories = 10, dt = 0.1)
             @test sol.converged
         end
