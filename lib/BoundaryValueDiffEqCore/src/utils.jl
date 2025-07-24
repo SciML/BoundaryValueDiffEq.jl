f16dc9a4b7dbeb00ac38a6553e6db220e267eae3
Qingyu Qu <52615090+ErikQQY@users.noreply.github.com>
2025-07-25 04:09:17 +0800

    Use proper Jacobian algorithm

--- lib/BoundaryValueDiffEqCore/src/utils.jl
+++ lib/BoundaryValueDiffEqCore/src/utils.jl
@@ -675,7 +675,7 @@ function __construct_internal_problem(prob::TwoPointBVProblem, alg, loss, jac,
         return __internal_nlsolve_problem(prob, resid_prototype, y, nlf, y, p)
     else
         optf = OptimizationFunction{true}(
-            __default_cost(prob.f), get_dense_ad(alg.diffmode),
+            __default_cost(prob.f), get_dense_ad(alg.jac_alg.diffmode),
             cons = loss, cons_j = jac, cons_jac_prototype = jac_prototype)
         lcons = zeros(T, N*M)
         ucons = zeros(T, N*M)
@@ -694,7 +694,26 @@ function __construct_internal_problem(prob, alg, loss, jac, jac_prototype,
         return __internal_nlsolve_problem(prob, resid_prototype, y, nlf, y, p)
     else
         optf = OptimizationFunction{true}(
-            __default_cost(prob.f), get_dense_ad(alg.diffmode),
+            __default_cost(prob.f), get_dense_ad(alg.jac_alg.diffmode),
+            cons = loss, cons_j = jac, cons_jac_prototype = jac_prototype)
+        lcons = zeros(T, N*M)
+        ucons = zeros(T, N*M)
+
+        return __internal_optimization_problem(
+            prob, optf, y, p; lcons = lcons, ucons = ucons)
+    end
+end
+function __construct_internal_problem(prob::TwoPointBVProblem, alg, loss, jac,
+        jac_prototype, resid_prototype, y, p, M::Int, N::Int,::Nothing)
+    T = eltype(y)
+    iip = SciMLBase.isinplace(prob)
+    if !isnothing(alg.nlsolve)
+        nlf = NonlinearFunction{iip}(loss; jac = jac, resid_prototype = resid_prototype,
+            jac_prototype = jac_prototype)
+        return __internal_nlsolve_problem(prob, resid_prototype, y, nlf, y, p)
+    else
+        optf = OptimizationFunction{true}(
+            __default_cost(prob.f), get_dense_ad(alg.jac_alg.nonbc_diffmode),
             cons = loss, cons_j = jac, cons_jac_prototype = jac_prototype)
         lcons = zeros(T, N*M)
         ucons = zeros(T, N*M)
