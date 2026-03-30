e0c6dafb3cd2f1206a92c9d6818f0ae070de8b47
Qingyu Qu <2283984853@qq.com>
2026-03-30 12:59:08 +0800

    Set adaptive=false to avoid adaptivity in test

--- lib/BoundaryValueDiffEqMIRK/test/mirk_basic_tests.jl
+++ lib/BoundaryValueDiffEqMIRK/test/mirk_basic_tests.jl
@@ -627,10 +627,10 @@ end
     ]
 
     bvp1 = TwoPointBVProblem(f!, (bca!, bcb!), u_guess, tspan; bcresid_prototype = (zeros(2), zeros(1)))
-    sol1 = solve(bvp1, MIRK4(), dt = 0.1, nlsolve_kwargs = (; maxiters = 0))
+    sol1 = solve(bvp1, MIRK4(), dt = 0.1, adaptive = false, nlsolve_kwargs = (; maxiters = 0))
     @test sol1.u == u_guess
 
     bvp2 = TwoPointBVProblem(f!, (bca!, bcb!), sol1, tspan; bcresid_prototype = (zeros(2), zeros(1)))
-    sol2 = solve(bvp2, MIRK4(), dt = 0.1, nlsolve_kwargs = (; maxiters = 0))
+    sol2 = solve(bvp2, MIRK4(), dt = 0.1, adaptive = false, nlsolve_kwargs = (; maxiters = 0))
     @test sol2.u == u_guess
 end
